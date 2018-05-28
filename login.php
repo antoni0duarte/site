6be443c064fb70c281b15a3bcf90f061d75dea6e
Antonio Duarte <antonioolucass@gmail.com>
2018-05-28 15:56:48 -0300

    Site

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Admin Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center" style="background-color:#3f3f3f">
+    <form class="form-signin" method="post" action="valida.php">
+      <img class="mb-4" src="../img/logo_m.png" alt="" width="auto" height="300px">
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email" name="usuario" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Senha" required>
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      <p class="mt-5 mb-3 " style="color:white;">Antonio Duarte &reg; 2018</p>
+    </form>
+  </body>
+</html>
